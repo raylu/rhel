77005656b66ab364b0e8f5b73a6c1f1831e57afe
raylu <lurayl@gmail.com>
2013-11-27 05:31:59 -0800

    set up WPF project

---
+++ rhel/MainWindow.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net;
+using System.Text;
+using System.Windows;
+
+namespace rhel {
+	public partial class MainWindow : Window {
+		public MainWindow() {
+			InitializeComponent();
+		}
+	}
+}
